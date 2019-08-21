c82de4c4a0d6cf44343c37ed2ff8b2b8fee211c1
K Blackmon <keitharuni@msn.com>
2019-08-21 09:57:16 -0700

    updating our app with shop page, collection-preview component, collection-item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import "./menu-item.styles.scss";
 import { withRouter } from "react-router-dom";
 
-const MenuItem = ({ title, id, image, size, history, linkUrl, match }) => (
+const MenuItem = ({ title, id, imageUrl, size, history, linkUrl, match }) => (
   <div
     className={`${size} menu-item`}
     onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div
       className='background-image'
-      style={{ backgroundImage: `url(${image})` }}
+      style={{ backgroundImage: `url(${imageUrl})` }}
     />
     <div className='content'>
       <h1 className='title'>{title}</h1>
