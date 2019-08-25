480ab7f41b14c0f6fc22b7ba1bf2dd58cce7c406
K Blackmon <keitharuni@msn.com>
2019-08-25 12:11:05 -0700

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,14 +1,32 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import CustomButton from "../custom-button/custom-button.component";
+import { addItem } from "../../redux/cart/cart.actions";
+
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({ imageUrl, id, name, price }) => (
-  <div className='collection-item'>
-    <div style={{ backgroundImage: `url(${imageUrl})` }} className='image' />
-    <div className='collection-footer'>
-      <span className='name'>{name}</span>
-      <span className='price'>{price}</span>
+const CollectionItem = ({ addItem, item }) => {
+  const { imageUrl, name, price } = item;
+  return (
+    <div className='collection-item'>
+      <div style={{ backgroundImage: `url(${imageUrl})` }} className='image' />
+      <div className='collection-footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        ADD TO CART
+      </CustomButton>
     </div>
-  </div>
-);
+  );
+};
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CollectionItem);
