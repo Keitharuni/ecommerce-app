19e1bdf30a44073156b4d10a264e5934be3c0820
K Blackmon <keitharuni@msn.com>
2019-08-28 17:30:31 -0700

    updating our application with reselect, replaced cart selectors with memoize d selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,19 +2,25 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({ toggleCartHidden }) => (
-  <div className='cart-icon' onClick={toggleCartHidden}>
-    <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>0</span>
-  </div>
-);
+const CartIcon = ({ toggleCartHidden, itemCount }) => {
+  console.log(itemCount);
+  return (
+    <div className='cart-icon' onClick={toggleCartHidden}>
+      <ShoppingIcon className='shopping-icon' />
+      <span className='item-count'>{itemCount}</span>
+    </div>
+  );
+};
 
-const mapStateToProps = ({ cart }) => ({ hidden: cart.hidden });
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state)
+});
 
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,20 @@
+import { createSelector } from "reselect";
+
+//input selectors
+const selectCart = state => state.cart;
+
+//export selectors
+export const selectCartItems = createSelector(
+  //array of input selectors first argument(could also be an export selector), second arg function return value we want out of selector can have more than one input selector as first argument [selectCart, selectUser], (cart, user)
+  [selectCart],
+  cart => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumlatedQuantity, cartItem) => accumlatedQuantity + cartItem.quantity,
+      0
+    )
+);
