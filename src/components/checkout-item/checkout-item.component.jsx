a8ccf1fb03a2730eeaa54b1db98f5c5969e4a6e0
K Blackmon <keitharuni@msn.com>
2019-08-29 12:26:41 -0700

    added addItem and decreaseItem funcionality to checkout item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,19 +1,45 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import "./checkout-item.styles.scss";
+import {
+  removeItemFromCart,
+  addItem,
+  decreaseItem
+} from "../../redux/cart/cart.actions";
 
-const CheckoutItem = ({ cartItem: { imageUrl, name, quantity, price } }) => {
+const CheckoutItem = ({ addItem, clearItem, decreaseItem, cartItem }) => {
+  const { imageUrl, name, quantity, price } = cartItem;
   return (
     <div className='checkout-item'>
       <div className='image-container'>
         <img alt='item' src={imageUrl} />
       </div>
       <span className='name'>{name}</span>
-      <span className='quantity'>{quantity}</span>
+      <span className='quantity'>
+        <div className='arrow' onClick={() => decreaseItem(cartItem)}>
+          &#10094;
+        </div>
+        <span className='value'>{quantity}</span>
+        <div className='arrow' onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
       <span className='price'>${price}</span>
-      <div className='remove-button'>&#10005;</div>
+      <div className='remove-button' onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </div>
     </div>
   );
 };
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(removeItemFromCart(item)),
+  addItem: item => dispatch(addItem(item)),
+  decreaseItem: item => dispatch(decreaseItem(item))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CheckoutItem);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,23 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
   }
 };
+
+export const decreaseItem = (cartItems, cartItemToRemove) => {
+  const exisitingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  if (exisitingCartItem.quantity === 1) {
+    return removeItemFromCart(cartItems, cartItemToRemove);
+  } else {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToRemove.id
+        ? { ...cartItem, quantity: cartItem.quantity - 1 }
+        : { ...cartItem }
+    );
+  }
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+};
